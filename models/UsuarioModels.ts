ea4ee78990b97f04ac9a71f5cacc7b301e241bea
Kelly Rodrigues <122828003+kellydatabase@users.noreply.github.com>
2023-02-16 20:51:40 -0300

    ajustes nos código

--- models/UsuarioModels.ts
+++ models/UsuarioModels.ts
@@ -5,7 +5,13 @@ const UsuarioShema = new Schema({
     email: {type : String, required : true},
     senha: {type : String, required : true},
     avatar: {type : String, required: false},
-       
+    cep: {type : Number, required: true},
+    endereço: {type : String, required: true},
+    num_endereço: {type : String, required: true},
+    complemento_endereço: {type : String, required: true},
+    bairro: {type : String, required: true},
+    cidade: {type : String, required: true},
+    estado: {type : String, required: true},
 });
 
 export const UsuarioModel = (mongoose.models.usuarios ||
--- types/CadastroRequisicao.ts
+++ types/CadastroRequisicao.ts
@@ -2,4 +2,13 @@ export type CadastroRequisicao = {
     nome: string,
     email: string,
     senha: string
-}
+    avatar: String,
+    telefone:Number,
+    cep: Number,
+    endereço: String,
+    num_endereço: String,
+    complemento_endereço: String,
+    bairro: String,
+    cidade: String,
+    estado: String
+};
